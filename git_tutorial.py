a45f37b94c17a7193ecec8955b3d7d5e8809f83e
markadut <mark_adut@brown.edu>
2021-09-26 19:38:01 -0400

    commit #2

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,1 +1,17 @@
 print("Hello World")
+
+print("This is a git tutorialfor data1050 hw")
+
+def odd_number_and_mean_finder(l):
+    #function that detects even numbers in a given list input 
+    odd_numbers = []
+    counter = 0
+    for x in l:
+        counter += x
+        if (x % 2)!= 0:
+            odd_numbers.append(x)
+            mean  = counter/len(l)
+    print(odd_numbers)
+    print(mean)
+
+odd_number_and_mean_finder([1, 2, 3, 4, 5, 6, 7, 8, 9])
